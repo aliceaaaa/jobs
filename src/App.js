5354d243951874dc31bcf86f95fcd5ca7da84261
Alice <a.andrianova@protoware.com>
2018-10-04 14:48:39 +0300

    Add Navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import { createBottomTabNavigator } from "react-navigation";
+
+import { AuthScreen, WelcomeScreen } from "./screens";
 
 export default class App extends Component {
   render() {
+    const MainNavigator = createBottomTabNavigator({
+      welcome: { screen: WelcomeScreen },
+      auth: { screen: AuthScreen }
+    });
+
     return (
       <View>
-        <Text>HELLO</Text>
+        <WelcomeScreen />
       </View>
     );
   }
---
+++ src/screens/index.js
@@ -0,0 +1,15 @@
+import AuthScreen from "./AuthScreen";
+import WelcomeScreen from "./WelcomeScreen";
+import MapScreen from "./MapScreen";
+import DeckScreen from "./DeckScreen";
+import ReviewScreen from "./ReviewScreen";
+import SettingshScreen from "./SettingsScreen";
+
+export {
+  AuthScreen,
+  WelcomeScreen,
+  MapScreen,
+  DeckScreen,
+  ReviewScreen,
+  SettingshScreen
+};
