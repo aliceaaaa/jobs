bad721a97ca711b3d3351a6a011d7f6444800d1b
Alice <a.andrianova@protoware.com>
2018-10-05 19:00:45 +0300

    add

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import MapView from "react-native-maps";
 
 export default class MapScreen extends Component {
   render() {
     return (
-      <View>
-        <Text>Map Screen</Text>
+      <View style={{ flex: 1 }}>
+        <MapView style={{ flex: 1 }} />
       </View>
     );
   }
