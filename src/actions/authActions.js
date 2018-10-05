4160c0eaf9b8bc4235f26af96f4415230d7d3239
Alice <a.andrianova@protoware.com>
2018-10-05 12:35:02 +0300

    Facebook integration

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,3 +1,29 @@
-import { FACEBOOK_LOGIN_SUCCESS } from "./types";
+import { AcyncStorage } from "react-native";
+import FBSDK from "react-native-fbsdk";
 
-export const facebookLogin = () => {};
+import { FACEBOOK_LOGIN_SUCCESS, FACEBOOK_LOGIN_FAIL } from "./types";
+
+const { LoginManager } = FBSDK;
+
+export const facebookLogin = () => async dispatch => {
+  let token = await AcyncStorage.getItem("fb_token");
+  if (token) {
+    dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
+  } else {
+    doFacebookLogin(dispatch);
+  }
+};
+
+doFacebookLogin = async dispatch => {
+  let { type, token } = await LoginManager.logInWithReadPermissionsAsync(
+    "688779421503816",
+    {
+      permissions: ["public_profile"]
+    }
+  );
+  if (type === "cancel") {
+    return dispatch({ type: FACEBOOK_LOGIN_FAIL });
+  }
+  await AsyncStorage.setItem("fb_token", token);
+  dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
+};
--- src/screens/AuthScreen.js
+++ src/screens/AuthScreen.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import { View, Text } from "react-native";
+import * as actions from "../actions";
 
-export default class AuthScreen extends Component {
+class AuthScreen extends Component {
+  componentDidMount() {
+    this.props.facebookLogin();
+  }
   render() {
     return (
       <View>
-        <Text>Auth Screen</Text>
-        <Text>Auth Screen</Text>
-        <Text>Auth Screen</Text>
-        <Text>Auth Screen</Text>
-        <Text>Auth Screen</Text>
+        <Text>sdfsfsd</Text>
       </View>
     );
   }
 }
+
+export default connect(
+  null,
+  actions
+)(AuthScreen);
